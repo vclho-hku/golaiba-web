b0a02e4576e55dfafb9fb0601f3ce6313c4e46a8
Vincent Ho <vincent.ho@shoplineapp.com>
2020-10-14 18:08:11 +0800

    remove file

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,23 +2,11 @@ import React from 'react';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-import ProTip from '../components/ProTip';
-// import Link from '../src/Link';
-import Copyright from '../components/Copyright';
 
 export default function Index() {
   return (
-    <Container maxWidth="sm">
-      <Box my={4}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Next.js example
-        </Typography>
-        {/* <Link href="/about" color="secondary">
-          Go to the about page
-        </Link> */}
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
+    <div>
+      body
+    </div>
   );
 }
