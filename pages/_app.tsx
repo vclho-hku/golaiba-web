ff49b9502c8905a5d5f2c44a0ef238ad770e56a1
Vincent Ho <vincent.ho@shoplineapp.com>
2020-10-15 17:01:55 +0800

    temp move

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
 import type { AppProps } from 'next/app'
-import SiteLayout from '../components/SiteLayout'
+import SiteLayout from '../Components/SiteLayout'
 
 function MyApp({ Component, pageProps }: AppProps) {
   React.useEffect(() => {
