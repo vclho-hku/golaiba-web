32b28eba44267434ea651adcf9ad065b59e6d384
Vincent Ho <vincent.ho@shoplineapp.com>
2020-10-20 14:54:13 +0800

    add firebase logic

---
+++ src/Firebase/firebase.tsx
@@ -0,0 +1,36 @@
+import app from 'firebase/app';
+import 'firebase/auth';
+
+const config = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
+
+class Firebase {
+  auth: any;
+
+  constructor() {
+    app.initializeApp(config);
+    this.auth = app.auth();
+  }
+
+  doCreateUserWithEmailAndPassword = (email: string, password: string) =>
+    this.auth.createUserWithEmailAndPassword(email, password);
+
+  doSignInWithEmailAndPassword = (email: string, password: string) =>
+    this.auth.signInWithEmailAndPassword(email, password);
+
+  doSignOut = () => this.auth.signOut();
+
+  doPasswordReset = (email: string) => this.auth.sendPasswordResetEmail(email);
+
+  doPasswordUpdate = (password: string) =>
+    this.auth.currentUser.updatePassword(password);
+}
+export default Firebase;
---
+++ src/Session/withAuthentication.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import AuthUserContext from './context';
+import { withFirebase } from '../Firebase';
+
+const withAuthentication = (Component: any) => {
+  class WithAuthentication extends React.Component<{ firebase: any }, any> {
+    constructor(props: any) {
+      super(props);
+      this.state = {
+        authUser: null,
+      };
+    }
+    listener: any = null;
+    componentDidMount() {
+      this.listener = this.props.firebase.auth.onAuthStateChanged(
+        (authUser: any) => {
+          authUser
+            ? this.setState({ authUser })
+            : this.setState({ authUser: null });
+        },
+      );
+    }
+    componentWillUnmount() {
+      this.listener();
+    }
+    render() {
+      return (
+        <AuthUserContext.Provider value={this.state.authUser}>
+          <Component {...this.props} />
+        </AuthUserContext.Provider>
+      );
+    }
+  }
+  return withFirebase(WithAuthentication);
+};
+export default withAuthentication;
