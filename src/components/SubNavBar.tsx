05848549a796a58ada1f15f171cd2654ce88da5b
Vincent Ho <hclvclho@gmail.com>
2021-05-05 05:51:50 +0800

    subnavbar routable

--- src/components/SubNavBar.tsx
+++ src/components/SubNavBar.tsx
@@ -3,6 +3,7 @@ import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import Link from 'next/link';
 
 const useStyles = makeStyles({
   root: {
@@ -12,7 +13,7 @@ const useStyles = makeStyles({
 
 const SubNavBar = (props: any) => {
   const classes = useStyles();
-  const [value, setValue] = React.useState(0);
+  const [value, setValue] = React.useState(-1);
 
   const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
     setValue(newValue);
@@ -21,13 +22,14 @@ const SubNavBar = (props: any) => {
   return (
     <Paper className={classes.root}>
       <Tabs
-        value={value}
         onChange={handleChange}
         indicatorColor="primary"
         textColor="primary"
         centered
       >
-        <Tab label="最新精選" />
+        <Link href={`/`}>
+          <Tab label="最新精選" />
+        </Link>
         <Tab label="名人書櫃" />
         <Tab label="經典書籍" />
         <Tab label="看書好地方" />
